6d0a4304da9d7904499e258ad33d63599ce68823
cududoma <cudu.doma@gmail.com>
2018-03-06 14:48:59 +0300

    complete task 2

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/local/bin/node
+import greeting from '../index.js';
 
 console.log('Welcome to the Brain Games!');
+greeting();
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+  const nameGamer = readlineSync.question('May I have your name? ');
+  const answer = 'Hello, ' + nameGamer + '!';
+  console.log(answer);
+};
+
+export default greeting;
