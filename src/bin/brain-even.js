a21b6890db448b0758ddd535723dd70d101beb57
cududoma <cudu.doma@gmail.com>
2018-03-07 22:23:22 +0300

    add brain-even

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import greeting from '../index';
+import greetThePlayer from '../index';
 
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomInt = (min, max) => Math.floor(Math.random() * ((max - min) + 1)) + min;
 
 const checkEven = (num) => {
   if (num % 2 === 0) {
-  	return 'yes';
+    return 'yes';
   }
-	return 'no';
+  return 'no';
 };
 
-const logicGame = (count) => {
+const gameLogic = (count, namePlayer) => {
   const randomInt = getRandomInt(1, 99);
   const question = `Question: ${randomInt}`;
   console.log(question);
@@ -21,19 +21,18 @@ const logicGame = (count) => {
   if (answer === checkEven(randomInt)) {
     if (count < 3) {
       console.log('Correct!');
-      count += 1;
-      logicGame(count);
-    } else return console.log(`Correct! \nCongratulations, ${nameGamer}!`);
+      gameLogic(count + 1);
+    } else {
+      console.log(`Correct! \nCongratulations, ${namePlayer}!`);
+    }
   } else {
-    return console.log(`${answer} is wrong answer ;(. Correct answer was ${checkEven(randomInt)}. \nLet's try again, ${nameGamer}!`);
+    console.log(`${answer} is wrong answer ;(. Correct answer was ${checkEven(randomInt)}. \nLet's try again, ${namePlayer}!`);
   }
 };
-
 
 console.log('Welcome to the Brain Games!');
 console.log('Answer "yes" if number even otherwise answer "no". \n');
 
-const nameGamer = greeting();
+const namePlayer = greetThePlayer();
 const count = 1;
-logicGame(count);
-
+gameLogic(count, namePlayer);
