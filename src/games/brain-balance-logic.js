8d425399aca740590f9f6ecdde8aadc7fa780b54
cududoma <cudu.doma@gmail.com>
2018-03-09 16:42:44 +0300

    add game-balance

---
+++ src/games/brain-balance-logic.js
@@ -0,0 +1,46 @@
+import { getRandomInt, commonGameLogic } from '../index';
+
+const minIndex = (digits) => {
+  let index = 0;
+  digits.forEach((d, i) => {
+    if (d < digits[index]) index = i;
+  });
+  return index;
+};
+
+const maxIndex = (digits) => {
+  let index = 0;
+  digits.forEach((d, i) => {
+    if (d > digits[index]) index = i;
+  });
+  return index;
+};
+
+const isBalanced = (digits) => {
+  const min = digits[minIndex(digits)];
+  const max = digits[maxIndex(digits)];
+  return (max - min) <= 1;
+};
+
+const balanceNumber = (num) => {
+  const digits = String(num).split('').map(c => Number(c));
+  while (!isBalanced(digits)) {
+    const max = maxIndex(digits);
+    const min = minIndex(digits);
+    digits[max] -= 1;
+    digits[min] += 1;
+  }
+  return digits.sort().map(d => String(d)).join('');
+};
+
+const balanceLogic = () => {
+  const number = getRandomInt(100, 9999);
+  console.log(`Question: ${number}`);
+  return balanceNumber(number);
+};
+
+const balanceGame = () => {
+  commonGameLogic('Balance the given number.', balanceLogic);
+};
+
+export default balanceGame;
