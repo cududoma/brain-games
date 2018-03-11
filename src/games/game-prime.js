7771f672d89b20f68af34208ebc84a3e06ae71b9
cududoma <cudu.doma@gmail.com>
2018-03-11 15:03:02 +0300

    check function of prime numbers - predicate

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,19 +1,19 @@
 import { getRandomInt, commonGameLogic } from '../index';
 
-const checkPrime = (num) => {
+const isPrime = (num) => {
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if ((num % i) === 0) {
-      return 'no';
+      return true;
     }
   }
-  return 'yes';
+  return false;
 };
 
 const primeGameCondition = () => {
   const randomInt = getRandomInt(1, 99);
   const question = `${randomInt}`;
 
-  const answer = checkPrime(randomInt);
+  const answer = isPrime(randomInt) ? 'yes' : 'no';
   return [question, answer];
 };
 
